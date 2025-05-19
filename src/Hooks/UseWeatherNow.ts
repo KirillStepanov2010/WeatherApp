f135ff9908d6bd8064e0e486294b8f4f6c41298b
Kirill Stepanov <kirill.stepanov2672@gmail.com>
2025-05-19 18:28:49 +0300

    Открыл апишку

--- src/Hooks/UseWeatherNow.ts
+++ src/Hooks/UseWeatherNow.ts
@@ -6,7 +6,10 @@ export const getWeatherNow = async (city: Ref<string>) => {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${
         city.value
-      }&units=metric&appid=${import.meta.env.VITE_OPENWEATHER_API_KEY}&lang=ru`
+      }&units=metric&appid=${
+        import.meta.env.VITE_OPENWEATHER_API_KEY ||
+        "45e866f213315fa5f88b4f5daa5c4f94"
+      }&lang=ru`
     )
     if (!response.ok) {
       throw new Error(`HTTP ошибка, ${response.status}`)
